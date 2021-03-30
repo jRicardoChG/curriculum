615957a806bd7607dca236f339fb756fa30743a5
ricardo <richago29@gmail.com>
2021-03-29 23:34:52 -0500

    finalizando merge

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> 165ca5fb2dbd0b51c22f7947eb1da44fd29c86c7
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,11 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-<<<<<<< HEAD
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
->>>>>>> 165ca5fb2dbd0b51c22f7947eb1da44fd29c86c7
