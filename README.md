9624a89ac249712235458f08ffe253c584d954d1
ricardo <richago29@gmail.com>
2021-03-29 19:04:41 -0500

    agrego orden a proyecto

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-Primer commit del proyecto
-=======
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Componentes/AreaDetalles/AreaDetalles.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AreaDetalles extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
